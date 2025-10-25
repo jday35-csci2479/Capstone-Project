0a693514c5fdcacaea5a7b412c1803139b188969
Joshua Day <jday35@student.cscc.edu>
2025-10-24 21:02:59 -0400

    Added routes, multiple form views, and Tailwind styling

--- Capstone_Project/resources/views/layouts/navigation.blade.php
+++ Capstone_Project/resources/views/layouts/navigation.blade.php
@@ -5,20 +5,24 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('home') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                    <x-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                        {{ __('Home') }}
+                    </x-nav-link>
+
+                    <x-nav-link :href="route('forums')" :active="request()->routeIs('forums')">
+                        {{ __('Forums') }}
                     </x-nav-link>
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
+            <!-- Dropdown Menu -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
@@ -35,7 +39,7 @@
 
                     <x-slot name="content">
                         <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                            {{ __('Account Settings') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
@@ -67,8 +71,8 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
+            <x-responsive-nav-link :href="route('home')" :active="request()->routeIs('home')">
+                {{ __('Home') }}
             </x-responsive-nav-link>
         </div>
 
@@ -81,7 +85,7 @@
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('Settings') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
--- Capstone_Project/routes/web.php
+++ Capstone_Project/routes/web.php
@@ -7,14 +7,29 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+// Forums Routes
+
+// Forum portal page
+Route::get('/forums', function () {
+    return view('forums.portal');
+})->name('forums');
+
+// Individual forum pages
+Route::view('/forums/generalDiscussion', 'forums.generalDiscussion')->name('forums.generalDiscussion');
+Route::view('/forums/homeworkHelp', 'forums.homeworkHelp')->name('forums.homeworkHelp');
+Route::view('/forums/offTopic', 'forums.offTopic')->name('forums.offTopic');
+Route::view('/forums/support', 'forums.support')->name('forums.support');
+Route::view('/forums/introductions', 'forums.introductions')->name('forums.introductions');
+Route::view('/forums/events', 'forums.events')->name('forums.events');
 
 require __DIR__.'/auth.php';
